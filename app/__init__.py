746ed8c45a93539ba0d435d709c981a50ad05a7f
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2022-09-21 21:25:00 -0400

    Created experience route and page

--- app/__init__.py
+++ app/__init__.py
@@ -31,3 +31,26 @@ def hobbiesPage():
         "hobbyData": hobbyData
     }
     return render_template('hobbies.html', title="MLH Fellow - Hobbies", url=os.getenv("URL"), **context)
+
+# Hobbies data
+experienceData = [
+    {"company-img": "static/img/Portrait_Placeholder.png",
+    "job-title": "Site-Reliability Engineer | Fellow @ MLH",
+    "position-type": "Fellowship",
+    "date-worked": "Sept 2022 - Dec 2022", 
+    "location": "United States",
+    "description": [
+        "Collaborated with some incredible people, amazing pod leader, and great mentor company.", 
+        "Worked on a portfolio project, creating a reusable template for the future.", 
+        "Used Python, Flask and Jinja, Bootstrap for styling "
+        ],
+    },
+]
+
+# Work Experience
+@app.route('/experience')
+def experiencePage():
+    context={
+        "experienceData": experienceData
+    }
+    return render_template('experience.html', title="MLH Fellow - Experience", url=os.getenv("URL"), **context)
