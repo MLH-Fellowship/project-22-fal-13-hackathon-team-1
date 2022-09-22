7a3574359bffaf49b837fb6524a2bb4358a7d5f8
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2022-09-21 22:09:32 -0400

    Added simple experience sections and filler data

--- app/__init__.py
+++ app/__init__.py
@@ -34,11 +34,36 @@ def hobbiesPage():
 
 # Hobbies data
 experienceData = [
-    {"company-img": "static/img/Portrait_Placeholder.png",
-    "job-title": "Site-Reliability Engineer | Fellow @ MLH",
-    "position-type": "Fellowship",
-    "date-worked": "Sept 2022 - Dec 2022", 
+    {"company_img": "static/img/favicon.ico",
+    "company_name": "Major League Hacking",
+    "job_title": "Site-Reliability Engineer | Fellow @ MLH",
+    "position_type": "Fellowship",
+    "date_worked": "Sep 2022 - Dec 2022", 
     "location": "United States",
+    "description": [
+        "Collaborated with some incredible people, amazing pod leader, and great mentor company.", 
+        "Worked on a portfolio project, creating a reusable template for the future.", 
+        "Used Python, Flask and Jinja, Bootstrap for styling "
+        ],
+    },
+    {"company_img": "static/img/MLBLogo.png",
+    "company_name": "Major League Baseball",
+    "job_title": "Site-Reliability Engineer",
+    "position_type": "Full-time",
+    "date_worked": "Sep 2022 - Present", 
+    "location": "New York, New York",
+    "description": [
+        "Collaborated with some incredible people, amazing pod leader, and great mentor company.", 
+        "Worked on a portfolio project, creating a reusable template for the future.", 
+        "Used Python, Flask and Jinja, Bootstrap for styling "
+        ],
+    },
+    {"company_img": "static/img/logo-Meta.png",
+    "company_name": "Meta",
+    "job_title": "Production Engineer",
+    "position_type": "Full-time",
+    "date_worked": "Jun 2020 - Aug 2022", 
+    "location": "Menlo Park, California",
     "description": [
         "Collaborated with some incredible people, amazing pod leader, and great mentor company.", 
         "Worked on a portfolio project, creating a reusable template for the future.", 
