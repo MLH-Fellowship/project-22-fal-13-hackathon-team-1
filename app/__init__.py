8bbdead39776de09da964ac359929f0471940e34
Tara Larsen <talarsen74@gmail.com>
2022-09-23 12:17:28 -0700

    working locations

--- app/__init__.py
+++ app/__init__.py
@@ -35,21 +35,25 @@ def hobbiesPage():
         "hobbyData": hobbyData
     }
     return render_template('hobbies.html', title="MLH Fellow - Hobbies", url=os.getenv("URL"), **context)
+locationData = [
+    {"country": "Canada", "lat": 45.421532, "long": -75.697189},
+    {"country": "Argentina", "lat": -34.603683, "long": -58.381557},
+    {"country": "Mexico", "lat": 19.432608, "long": -99.133209},
+    {"country": "US", "lat": 38.9071923, "long": -77.0368707},
+    {"country": "Brazil", "lat": -15.793889, "long": -47.882778},
+    {"country": "Panama", "lat": 8.983333, "long": -79.516670},
+    {"country": "Dominican Republic", "lat": 18.483402, "long": -69.929611},
+    {"country": "Jamaica", "lat": 18.017874, "long": -76.809904},
+    {"country": "Paraguay", "lat": -25.263740, "long": -57.575926},
+    {"country": "UK", "lat": 51.509865, "long": -0.118092},
+    {"country": "France", "lat": 48.864716, "long": 2.349014},
+    {"country": "Spain", "lat": 40.416775, "long": -3.703790}
+]
 
 #Location route
-@app.route("/locations/<name>")
-def mapview(name):
-    data = load_profiles_from_json('locationData.json')
-    if name in data:
-        info = data[name]
-        return render_template('locations.html', title="MLH Fellow - Locations",name=name, info=info, url=os.getenv("URL"), API_KEY=os.getenv("API_KEY"))
-    else:
-        return landingPage()
-    
-def load_profiles_from_json(filename) -> dict:
-    # Get the relative path for the JSON data
-    path = f'{os.getcwd()}/{filename}'
-    # Open the file and return its parsed contents
-    # UTF-8 encoding is used to parse apostrophes correctly
-    with open(path, "r", encoding='utf8') as file:
-        return json.load(file)
+@app.route('/locations/')
+def mapview():
+    context = {
+        "locationData": locationData
+    }
+    return render_template('locations.html', title="MLH Fellow - Locations", url=os.getenv("URL"), API_KEY=os.getenv("API_KEY"),  **context)
