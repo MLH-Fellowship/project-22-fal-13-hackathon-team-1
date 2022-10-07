5b1f6c44fc2d1b364ddec3e0637daa63a0072442
Tara Larsen <talarsen74@gmail.com>
2022-10-07 14:07:03 -0700

    Delete redeploy-site.sh

--- redeploy-site.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-tmux kill-session -a
-cd portfolio
-# make sure git repo in VPS has the latest changes from the main branch on GH
-git fetch && git reset origin/main --hard
-tmux new -d -s ta_portfolio
-tmux send -t tal_portfolio 'python -m venv python3-virtualenv' ENTER
-tmux send -t tal_portfolio 'pip3 install -r requirements.txt' ENTER
-tmux send -t tal_portfolio 'flask run --host=0.0.0.0' ENTER
