b55e064d62b050b06d07c9a09329a85e148f55e2
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2022-10-12 16:38:15 -0400

    Cleaning up .envs and adding api methods w/ peewee

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,68 @@ import json
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from flask_googlemaps import GoogleMaps, Map, icons
+#for sql connection
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict
 
 load_dotenv()
 app = Flask(__name__)
 
-os.getenv("API_KEY") 
+#MySQL DB
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+)
+print(mydb)
 
+#peewee model
+    #Create a "timeline post" data table
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+#Timeline data 
+    #POST
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    return model_to_dict(timeline_post)
+
+    #GET
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+    #DELETE by Username
+@app.route('/api/timeline_post', methods=['DELETE'])
+def delete_time_line_post():
+    # deleteUser = input("Enter Post User: ")
+    post = TimelinePost.get(TimelinePost.name == "Test")
+    post.delete_instance()
+    #return remaining posts
+    return print(f"Deleted User: {deleteUser}")
+
+#for '/data/json' to become dict
 jsonData = open('app/data/data.json')
-data = json.load(jsonData) # turns json into regular dict with obj
+data = json.load(jsonData)
 
 #Landing Page
 @app.route('/')
