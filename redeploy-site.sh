94f7c3cbbaae06104b1c59b0025d6993474d9b00
root <talarsen74@gmail.com>
2022-10-15 22:06:44 +0000

    add restart myportfolio service

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-tmux kill-session -t ta_portfolio
 cd portfolio
 # make sure git repo in VPS has the latest changes from the main branch on GH
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip3 install -r requirements.txt
-tmux new-session -d -s ta_portfolio 'flask run --host=0.0.0.0'
+cd ..
+cd /etc/systemd/system/myportfolio.service
+systemctl daemon-reload
+systemctl restart myporfolio
