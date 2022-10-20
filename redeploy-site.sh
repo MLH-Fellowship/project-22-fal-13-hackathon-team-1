20813b72e9fa10cdb32a6e3fd4163533a450633d
Tara Larsen <talarsen74@gmail.com>
2022-10-20 14:07:40 -0700

    fix redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+i#!/bin/bash
 cd portfolio
 # make sure git repo in VPS has the latest changes from the main branch on GH
 git fetch && git reset origin/main --hard
-python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip3 install -r requirements.txt
-cd ..
-cd /etc/systemd/system
-systemctl daemon-reload
 systemctl restart myportfolio
