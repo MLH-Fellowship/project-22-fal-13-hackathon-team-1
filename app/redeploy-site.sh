b1660a628e8b6a81c07a73cec5a3183c8d4f3379
bukunmig <bgesinde44@icloud.com>
2022-10-26 17:05:59 -0700

    update

--- app/redeploy-site.sh
+++ app/redeploy-site.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd portfolio
+cd project-22-fal-13-hackathon-team-1
 # make sure git repo in VPS has the latest changes from the main branch on GH
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
