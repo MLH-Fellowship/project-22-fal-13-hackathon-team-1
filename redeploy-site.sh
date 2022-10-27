d1055dee8615c7cafe3925fc7fb1d495f96d6fd8
bukunmig <bgesinde44@icloud.com>
2022-10-26 19:07:46 -0700

    edit

---
+++ redeploy-site.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# make sure git repo in VPS has the latest changes from the main branch on GH
+git fetch && git reset origin/main --hard
+source python3-virtualenv/bin/activate
+pip3 install -r requirements.txt
+systemctl restart myportfolio
