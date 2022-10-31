d969f4b127dd7f0169b3d11ef5f1f85bce99cef2
bukunmig <bgesinde44@icloud.com>
2022-10-31 06:59:44 -0700

    Test update: finalized db test

--- app/tests/test_db.py
+++ app/tests/test_db.py
@@ -2,11 +2,11 @@ import unittest
 from peewee import *
 
 from app import TimelinePost
-
+from playhouse.shortcuts import model_to_dict
 MODELS = [TimelinePost]
 
 # use an in-memory SQLite for tests
-test_db = SqliteDatabase(' :memory: ')
+test_db = SqliteDatabase(':memory:')
 
 class TestTimelinePost(unittest.TestCase):
     def setUp(self) :
@@ -16,7 +16,7 @@ class TestTimelinePost(unittest.TestCase):
 
     def tearDown(self):
         test_db.drop_tables (MODELS)
-
+        #close connection to database
         test_db.close()
     
     def test_timeline_post(self):
@@ -28,4 +28,15 @@ email='jame@example.com'
 , content='Hello world, I\'m Jane!')
         assert second_post. id == 2
 
-#TODO 
+#for assert statements
+get_timeline_posts = [
+    model_to_dict(item)
+    for item in TimelinePost
+]
+
+assert get_timeline_posts[0]['name'] == 'John Doe'
+assert get_timeline_posts[1]['name'] == 'Jane Doe'
+assert get_timeline_posts[0]['email'] == 'john@example.com'
+assert get_timeline_posts[1]['email'] == 'jane@example.com'
+assert get_timeline_posts[0]['content'] == 'Hello world, I\'m John!'
+assert get_timeline_posts[1]['content'] == 'Hello world, I\'m Jane'
