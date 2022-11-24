6408de8c01162f6f040944cece1f031237b89cc7
bukunmig <bgesinde44@icloud.com>
2022-11-23 16:53:27 -0800

    test workflow

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,4 +3,5 @@
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip3 install -r requirements.txt
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
